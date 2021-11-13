76d7bf24a48810bb37d51759794592b550c718bf
khan-shimul <khanshimul196@gmail.com>
2021-11-13 23:18:48 +0600

    add sdk

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ admin.initializeApp({
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.je3vw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-// verify firebase idToken
+// verify idToken
 async function verifyToken(req, res, next) {
     if (req.headers?.authorization?.startsWith('Bearer ')) {
         const token = req.headers.authorization.split('Bearer ')[1];
